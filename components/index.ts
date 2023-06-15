8f3738e3fce2b047ea22e3ffe9843ecb1a0520a0
carlos <crlsmelegrito@yahoo.com>
2023-06-15 15:30:52 +0800

    start

---
+++ components/index.ts
@@ -0,0 +1,20 @@
+// import CarCard from "./CarCard";
+import CustomButton from "./CustomButton";
+// import CustomFilter from "./CustomFilter";
+import Footer from "./Footer";
+import NavBar from "./Navbar";
+// import ShowMore from "./ShowMore";
+// import SearchBar from "./Searchbar";
+import Hero from "./Hero";
+// import ProductList from "./ProductList";
+// import ProductList2 from "./ProductList2";
+// import ProductList3 from "./ProductList3";
+
+
+
+export {
+  Hero,
+  CustomButton,
+  Footer,
+  NavBar,
+};
