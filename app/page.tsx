01eed23b453a4b147710b964b8ce80fa42a8d36c
carlos <crlsmelegrito@yahoo.com>
2023-06-17 18:19:59 +0800

    Working

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,11 @@ import Image from 'next/image'
 
 import {Hero, Hero2} from "@components";
 
-
 export default function Home() {
   return (
     <main className='overflow-hidden'>
-    <Hero />    
-    <Hero2 />    
+    <Hero />
+    {/* <Hero2 />     */}
 
     </main>
   )
